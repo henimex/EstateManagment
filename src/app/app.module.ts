f605cf8ab2a3a40940918fecbdb47cef97049de8
henimex <49072861+henimex@users.noreply.github.com>
2021-04-07 23:21:44 +0300

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule,
