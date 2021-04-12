e5f1205743b7fa011d56eee76ce05cbe969992ac
J.Henimex <J.Henimex@IT-01>
2021-04-12 16:09:58 +0300

    Form Refactoring

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -9,10 +9,14 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class UserRegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-    this.intilizeForm();
+    this.createRegisterForm();
+    //this.intilizeForm();
+    setTimeout(() => {
+      console.log("Timeout Test")
+    },2000)
   }
 
   intilizeForm(): void {
@@ -25,17 +29,36 @@ export class UserRegisterComponent implements OnInit {
     });
   }
 
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      userName:['', [Validators.required]],
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required,Validators.minLength(8)]],
+      cpassword:['', [Validators.required]],
+      mobile:['', [Validators.required,Validators.maxLength(10)]]
+    },{validator:this.passwordValidator})
+  }
+
   onSubmit(): void {
     this.ownPassMatch(this.registerForm)
     console.log("Submit Works")
     console.log(this.registerForm)
   }
 
-  passwordValidator(formGroup: FormGroup){
+  passwordValidator(formGroup: FormGroup):Validators{
     //pass validator not working on old init. will try with FormBuilder.
     return formGroup.get('password').value === formGroup.get('cpassword').value ? null :
     {notMatched:true}
   }
+
+  // pwValidator(): ValidatorFn {
+  //   return (control: AbstractControl): { [key: string]: boolean } => {
+  //       if (control.get('password').value === control.get('cpassword').value) {
+  //           return { 'notMatched': true };
+  //       }
+  //       return null;
+  //   };
+  // }
 
   ownPassMatch(ownGroup: FormGroup):Boolean {
     if (ownGroup.get('password').value === ownGroup.get('cpassword').value) {
